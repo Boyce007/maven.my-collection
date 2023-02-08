fe54e4fe541ffdb94ffa844d7ae665091c3b202a
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2023-02-07 19:38:48 -0500

    minor changes

--- src/main/java/com/github/curriculeon/MyLinkedList.java
+++ src/main/java/com/github/curriculeon/MyLinkedList.java
@@ -1,9 +1,46 @@
 package com.github.curriculeon;
 
-public class MyLinkedList<SomeType> {
+import java.util.Iterator;
+
+public class MyLinkedList<SomeType> implements MyCollectionInterface<SomeType>{
     public MyLinkedList() {
     }
 
     public MyLinkedList(SomeType... valuesToBePopulatedWith) {
+    }
+
+    @Override
+    public void add(SomeType objectToAdd) {
+
+    }
+
+    @Override
+    public void remove(SomeType objectToRemove) {
+
+    }
+
+    @Override
+    public void remove(int indexOfObjectToRemove) {
+
+    }
+
+    @Override
+    public SomeType get(int indexOfElement) {
+        return null;
+    }
+
+    @Override
+    public Boolean contains(SomeType objectToCheckFor) {
+        return null;
+    }
+
+    @Override
+    public Integer size() {
+        return null;
+    }
+
+    @Override
+    public Iterator<SomeType> iterator() {
+        return null;
     }
 }
--- src/main/java/com/github/curriculeon/MyMap.java
+++ src/main/java/com/github/curriculeon/MyMap.java
@@ -1,4 +1,33 @@
 package com.github.curriculeon;
 
-public class MyMap<KeyType, ValueType> {
+public class MyMap<KeyType, ValueType> implements MyMapInterface<KeyType,ValueType> {
+    @Override
+    public void put(KeyType key, ValueType value) {
+
+    }
+
+    @Override
+    public ValueType get(KeyType key) {
+        return null;
+    }
+
+    @Override
+    public MySet<KeyType> getKeySet() {
+        return null;
+    }
+
+    @Override
+    public MyCollectionInterface<ValueType> getValues() {
+        return null;
+    }
+
+    @Override
+    public MySet<KeyValue<KeyType, ValueType>> getList() {
+        return null;
+    }
+
+    @Override
+    public MyMapInterface<ValueType, KeyType> invert() {
+        return null;
+    }
 }
