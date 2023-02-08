40c780373aabef74a948cfa32a23a70996986dec
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2023-02-07 19:50:08 -0500

    minor changes

--- src/main/java/com/github/curriculeon/MyArrayList.java
+++ src/main/java/com/github/curriculeon/MyArrayList.java
@@ -3,10 +3,16 @@ package com.github.curriculeon;
 import java.util.Iterator;
 
 public class MyArrayList<SomeType> implements MyCollectionInterface<SomeType> {
+
+    private SomeType[] content;
+    private Integer index;
     public MyArrayList() {
+        index = 0;
+        content = (SomeType[]) new Object[10];
     }
 
     public MyArrayList(SomeType[] valuesToBePopulatedWith) {
+
     }
 
     @Override
